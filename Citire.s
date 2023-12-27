834356fcf52f9f8812a7e1fac40e24d636a32fb0
Meanderingg <149524587+Meanderingg@users.noreply.github.com>
2023-12-27 17:54:15 +0200

    Parcurgerea matricei

--- Citire.s
+++ Citire.s
@@ -8,8 +8,10 @@
 	j: .space 4
 
 	k: .space 4
+	nriter: .space 4
 
 	mat: .zero 1600 #initializeaza cu zero spatiul din memorie
+	mat2: .zero 1600
 
 	formatScanf: .asciz "%d"
 	formatPrintf: .asciz "%d "
@@ -47,10 +49,18 @@ et_p:
 	call scanf
 	addl $8, %esp 
 
+	movl mlines, %eax
+	cmp x, %eax
+	jle et_exit # verificarea datelor de intrare	
+
 	push $y
 	push $formatScanf
 	call scanf
 	addl $8, %esp 
+
+	movl nCells, %eax
+	cmp y, %eax
+	jle et_exit # verificarea datelor de intrare	
 
 	pop %ecx
 	
@@ -70,17 +80,72 @@ et_p:
 	
 main_continue:
 
+	push $k
+	push $formatScanf
+	call scanf
+	addl $8, %esp #citirea lui k
+
+	mov mlines, %eax
+	mull nCells
+	movl %eax, nriter #numarul de iteratii pt parcurgerea matricei	
+	xor %ecx, %ecx
+
+parcurgere_celule:
+
+	cmp k , %ecx
+	je et_cont #sa fac asta de k ori, mutarea matricei 
+		
+	movl $0, i
+	push %ecx #pt a retine nr generatiei curente
+for_parcurgere_linii:
+	movl i, %ecx
+	cmp mlines, %ecx
+	je parcurgere_celule_cont
+
+	movl $0, j
+for_parcurgere_coloane:
+	movl j, %ecx
+	cmp nCells, %ecx
+	je for_parcurgere_linii_cont	
+	
+	mov i, %eax
+	inc %eax #pt a sari peste linia de bordare
+
+	mov nCells, %ebx
+	add $2, %ebx
+	mull %ebx #se adauga 2 pt cele 2 coloane de bordare
+	
+	addl j, %eax
+	inc %eax #pt a doua coloana de bordare
+
+	movl (%edi, %eax, 4), %ebx 
+	xorl %edx, %edx
+	cmp %ebx, %edx
+	je caz_zero
+
+caz_unu:
+	
+numararea_vecinilor:
+	xorl %ebx, %ebx #tinem nr de vecini vii in ebx
+	add (%edi, %eax, 4), %ebx	
+	incl j
+	jmp for_parcurgere_coloane
+caz_zero:
+
+	incl j
+	jmp for_parcurgere_coloane
+for_parcurgere_linii_cont:
+	incl i
+	jmp for_parcurgere_linii	
+	
+parcurgere_celule_cont:	
+	pop %ecx
+	incl %ecx
+	jmp parcurgere_celule
 et_cont:
 
 	movl $0, i
 
-	movl mlines, %eax
-	add $2, %eax
-	movl %eax, mlines #pt afisarea bordarii
-
-	movl nCells, %eax
-	add $2, %eax
-	movl %eax, nCells #pt afisarea bordarii
 
 for_lines:
 	movl i, %ecx
@@ -93,8 +158,14 @@ for_columns:
 	je for_lines_cont
 	
 	mov i, %eax
-	mull nCells
+	inc %eax #pt a sari peste linia de bordare
+
+	mov nCells, %ebx
+	add $2, %ebx
+	mull %ebx #se adauga 2 pt cele 2 coloane de bordare
+	
 	addl j, %eax
+	inc %eax #pt a doua coloana de bordare
 
 	movl (%edi, %eax, 4), %ebx 
 	push %ebx
